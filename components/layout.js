2116fc5fa883b2142e06a2bdac75edd2368973ab
Rob Kennedy <63457697+ciderbet@users.noreply.github.com>
2020-06-18 16:16:00 +0100

    Add bootstrap syling to layout

--- components/layout.js
+++ components/layout.js
@@ -1,20 +1,31 @@
+import Head from 'next/head'
+
 const Layout = ({ children }) => {
+  const head = () => (
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossorigin="anonymous"
+    />
+  )
+
   const nav = () => (
-    <ul className="nav">
+    <ul className="nav nav-tabs bg-warning">
       <li className="nav-item">
-        <a className="nav-link" href="">Home</a>
+        <a className="nav-link text-dark" href="">Home</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="">Login</a>
+        <a className="nav-link text-dark" href="">Login</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="">Register</a>
+        <a className="nav-link text-dark" href="">Register</a>
       </li>
     </ul>
   )
 
   return <React.Fragment>
-    {nav()} {children}
+    {head()} {nav()} <div className="container pt-5 pb-5">{children}</div>
   </React.Fragment>
 }
 
