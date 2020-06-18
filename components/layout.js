17ebe35a9250cd11ec79f925dda470a7a9140b0d
Rob Kennedy <63457697+ciderbet@users.noreply.github.com>
2020-06-18 18:27:32 +0100

    add nav progress bar

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,23 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Router from 'next/router'
+import Nprogress from 'nprogress'
+
+Router.onRouteChangeStart = url => Nprogress.start()
+Router.onRouteChangeComplete = url => Nprogress.done()
+Router.onRouteChangeError = url => Nprogress.done()
 
 const Layout = ({ children }) => {
   const head = () => (
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-      crossorigin="anonymous"
-    />
+    <React.Fragment>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous"
+      />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"></link>
+    </React.Fragment>
   )
 
   const nav = () => (
